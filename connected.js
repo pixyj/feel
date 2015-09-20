695df2f44aab6ab5579f6d4081bc017a00877b82
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-09-20 20:44:07 +0530

    Concept Graph Visualization! Boy, I've been working on this problem for ever. So here's a first version of visualising nodes.  Next up, visualising edges.

---
+++ connected.js
@@ -0,0 +1,130 @@
+var graph = {
+
+    levels: [
+                [
+                    {
+                        name: "Projection of a vector on a subspace",
+                        chapterIndex: 1,
+                        id: 1
+                    }
+                ],
+
+                [
+                    {
+                        name: "Projection of a vector on a subspace",
+                        chapterIndex: 1,
+                        id: 4
+                    },
+                    {
+                        name: "Projection of a vector on a subspace. In another's arms. In another's arms. In another's arms.",
+                        chapterIndex: 1,
+                        id: 4
+                    }
+                ],
+
+                [
+                    {
+                        name: "Projection of a vector on a another",
+                        chapterIndex: 1,
+                        id: 4
+                    }
+                ]
+    ],
+
+    edges: [
+        {
+            from: 1,
+            to: 4
+        },
+        {
+            from: 4,
+            to: 8
+        }
+    ]
+};
+
+var getForeignObjectAttrs = function(levelIndex, position, levelConceptCount, levelWidth, levelHeight) {
+
+    var y = levelHeight;
+
+    var nodePlusMarginWidth = levelWidth/levelConceptCount;
+    var leftMarginWidth, rightMarginWidth;
+    leftMarginWidth = rightMarginWidth = nodePlusMarginWidth*0.2;
+    var nodeWidth = nodePlusMarginWidth - leftMarginWidth - rightMarginWidth;
+
+    x = leftMarginWidth + position*nodePlusMarginWidth;
+
+    attrs = {
+        x: x,
+        y: y,
+        width: nodeWidth
+    };
+
+    return attrs;
+};
+
+var drawNode = function(node, levelIndex, position, levelConceptCount, svgAttrs, levelHeight) {
+    console.log("Drawing ", node, " at level ", levelIndex, " in position", position);
+
+    var svg = svgAttrs.svg;
+    var svgWidth = svgAttrs.width;
+
+    var chapterClass = "chapter-box-" + node.chapterIndex;
+    var h4 = $("<h4>").html(node.name);
+    var p = $("<p>").addClass("concept-box").addClass(chapterClass).append(h4);
+
+    //foreignObject does not work on IE #todo. But my initial technical audience does not use IE, I guess? 
+    //And making aligning svg text is a pain
+    var attrs = getForeignObjectAttrs(levelIndex, position, levelConceptCount, svgWidth, levelHeight);
+    var f = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject')
+
+    f = $(f).append(p).attr(attrs);
+    svg.append(f);
+    return p.height();
+};
+
+var drawLevelNodes = function(level, levelIndex, svgAttrs, levelHeight) {
+
+    var i, length;
+    length = level.length;
+
+    var maxHeight = 0;
+    for(i = 0; i < length; i++) {
+        var node = level[i];
+        var height = drawNode(node, levelIndex, i, length, svgAttrs, levelHeight);
+        maxHeight = _.max([maxHeight, height]);
+    }
+
+    return maxHeight;
+};
+
+var drawAllNodes = function(levels, svgAttrs) {
+
+    var i, length;
+    length = levels.length;
+    var cumulativeLevelHeight = 30;
+    for(i = 0 ; i < length; i++) {
+        var level = levels[i];
+        maxLevelHeight = drawLevelNodes(level, i, svgAttrs, cumulativeLevelHeight);
+        cumulativeLevelHeight = cumulativeLevelHeight + maxLevelHeight + 30;
+    }
+};
+
+
+var init = function() {
+    
+    //cache the width so that it can used be in calculations without querying the dom each time.
+    var svg = $("svg");
+    svg.empty();
+    var svgAttrs = {
+        svg: svg,
+        width: svg[0].offsetWidth
+    }
+    drawAllNodes(graph.levels, svgAttrs);
+};
+
+
+$(document).ready(init);
+
+window.onresize = init;
+
