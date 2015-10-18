2116e62dac31fec296593fce1b39b28ae7d36e6c
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-10-19 04:56:50 +0530

    Modularized Problem Solving models

--- client/app/base/js/models.js
+++ client/app/base/js/models.js
@@ -1,33 +1,3 @@
-var PlanModel = Backbone.Model.extend({
 
-    defaults: {
-        planInput: "",
-        planDisplay: "",
-    },
-
-    //idAttribute: "planInput" -> Well, it's ok to have a plan in mind and not explictly write it, I guess. Especially for new users. 
-
-});
-
-var PlanCollection = Backbone.Collection.extend({
-    
-    model: PlanModel
-
-});
-
-var ProblemSolvingModel = Backbone.Model.extend({
-
-    defaults: {
-        understandProblemInput: "",
-        understandProblemDisplay: "",
-        result: null    
-    },
-
-
-    initialize: function() {
-        this.plans = new PlanCollection();
-    }
-
-});
 
 
