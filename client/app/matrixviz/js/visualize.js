15017e6495240e41141153a70c893b1dc10e39fb
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-11-08 11:05:02 +0530

    Matrix Visualization:  Added user configurable matrix multiplication.

--- client/app/matrixviz/js/visualize.js
+++ client/app/matrixviz/js/visualize.js
@@ -351,36 +351,52 @@ var SvgGridView = Backbone.View.extend({
 
 });
 
-// <p class="range-field">
-//   <input type="range" id="test5" min="0" max="100" />
-// </p>
-var RangeView = Backbone.View.extend({
-
-    el: "<p>",
-
-    MIN: 0,
-    MAX: 100,
+var SpeedView = Backbone.View.extend({
 
     initialize: function(options) {
         this.options = options;
+
+    },
+
+    SPEEDS: {
+        SLOW: 50,
+        MEDIUM: 20,
+        FAST: 7,
+        INSTANT: 0
     },
 
     render: function() {
-        
-        var input = $("<input />").attr({
-            type: "range",
-            min: this.options.min || this.MIN,
-            max: this.options.max || this.MAX 
-        });
 
+        var h4 = $("<h4>").addClass("matrix-options-heading").html("Visualization Speed");
+        this.$el.append(h4);
 
-        var thumb = $('<span class="thumb"><span class="value"></span></span>')
-        this.$el.addClass("range-field").append(input).append(thumb);
-        this.$el.css({
-            "margin-top": "30px"
-        });
-        
+        var speeds = _.keys(this.SPEEDS);
+
+        var length;
+        for(var i = 0, length = speeds.length; i < length; i++) {
+            var id = "speed-" + speeds[i];
+            var input = $("<input />").attr({
+                type: "radio",
+                id: id,
+                name: "speeds",
+                value: this.SPEEDS[speeds[i]]
+            });
+            var label = $("<label>").attr("for", id).html(speeds[i]).addClass("matrix-speed-label");
+            var p = $("<p>").append(input).append(label);
+            this.$el.append(p);
+        }
+
+        this.$el.find("#speed-MEDIUM").attr("checked", true);
+
         return this;
+    },
+
+    getState: function() {
+        var val = this.$el.find('input:radio[name=speeds]:checked').val();
+        var timeout = parseInt(val);
+        return {
+            timeout: timeout
+        }
     }
 
 });
@@ -389,6 +405,8 @@ var MatrixInputView = Backbone.View.extend({
 
     ROWS: 2,
     COLUMNS: 2,
+
+    el: "<table id='matrix-input-table'> </table>",
 
     initialize: function(options) {
 
@@ -409,35 +427,106 @@ var MatrixInputView = Backbone.View.extend({
 
     },
 
+
     render: function() {
-        this.views = [];
 
-        var table = $("<table>").addClass("matrix-input-table");
+        this.inputs = new Array(this.ROWS);
+
+        var table = this.$el;
+
         for(var i = 0; i < this.ROWS; i++) {
-
-            var rowViews = [];
-            var row = $("<tr>");
+            var columnViews = new Array(this.COLUMNS);
+            this.inputs[i] = columnViews;
+            var tr = $("<tr>");
             for(var j = 0; j < this.COLUMNS; j++) {
-
-                var input = $("<input>").attr({
-                    "type": "number",
-                    "value": this._matrix[i][j]
+                var td = $("<td>");
+                var input = $("<input />").attr({
+                    type: "number",
+                    value: this._matrix[i][j]
+                }).css({
+                    "border": "none"
                 });
-                rowViews.push(input);
-
-                var td = $("<td>").append(input);
-                row.append(td);
-
+                this.inputs[i][j] = input;
+                var div = $("<div>").append(input);
+                td.append(div);
+                this.styleColumn(td, i, j);
+                
+                tr.append(td);
             }
-            table.append(row);
-            this.views.push(rowViews);
+            table.append(tr);
         }
-        this.$el.append(table);
+        //this.$el.append(table);
+        this.renderBorders();
         return this;
     },
 
-    getInputMatrix: function() {
+    renderBorders: function() {
 
+        var top = this.options.top;
+        var positions = [
+            {
+                top: top,
+                left: 0,
+                "margin-left": "15px"
+            },
+            {
+                top: top,
+                right: 0,
+                "margin-right": "15px"
+            },
+            {
+                bottom: 0,
+                left: 0,
+                "margin-left": "15px"
+            },
+            {
+                bottom: 0,
+                right: 0,
+                "margin-right": "15px"
+            }
+        ];
+
+        var self = this;
+        positions.forEach(function(p) {
+            var div = $("<div>").addClass("matrix-input-table-border-div").css(p);
+            self.options.parent.append(div);
+        });
+    },
+
+    styleColumn: function(td, row, column) {
+
+        if(column === 0) {
+            td.find("input").css({
+                "margin-left": "10%"
+            });
+        }
+        else if( column === (this.COLUMNS - 1) ) {
+            td.css({
+                "width": "30%"
+            });
+            td.find("input").css({
+                "width": "100%"
+            });
+        }
+
+    },
+
+    getState: function() {
+
+        var inputs = this.$el.find("input");
+        var elements = new Array();
+        for(var i = 0; i < this.ROWS; i++) {
+
+            elements[i] = new Array(this.COLUMNS);
+            for(var j = 0; j < this.COLUMNS; j++) {
+                var val = this.inputs[i][j].val() || 0;
+                elements[i][j] = parseInt(val);
+            }
+        }
+
+        return {
+            transformationMatrix: elements
+        };
     },
 
     remove: function() {
@@ -450,41 +539,249 @@ var MatrixInputView = Backbone.View.extend({
     }
 });
 
-// var initColors = function() {
-//     var elem = document.getElementById("one");
-//     context = elem.getContext("2d");
-//     context.drawImage(document.getElementById("img"), 0, 0, 10, 10);
+var MatrixShapeOptionsView = Backbone.View.extend({
+    
 
-//     var imageData = context.getImageData(0, 0, 10, 10);
-//     pix = imageData.data;
+    initialize: function(options) {
+        this.options = options;
+    },
 
-//     var colors = [];
-//     var i, n;
-//     for(var i = 0, n = pix.length; i < n; i += 4) {
-//         var c = "#";
-//         c += pix[i].toString(16);
-//         c += pix[i+1].toString(16);
-//         c += pix[i+2].toString(16);
-//         colors.push(c.toUpperCase());
-//     };
+    render: function() {
 
-//     app.colors = colors;
-//     window.colors = colors;
-// };
+        var h4 = $("<h4>").addClass("matrix-options-heading").html("Input Matrix Shape");
+        this.$el.append(h4);
+        var svg = document.createElementNS(app.ns, "svg");
+        svg = $(svg);
+        this.$el.append(svg);
+
+        var height = this.options.height - h4.height();
+        svg.attr({
+            "width": this.options.width,
+            "height": height
+        });
+        
+        var size = _.min([this.options.width, height]);
+
+        var circle = document.createElementNS(app.ns, "circle");
+        circle = $(circle);
+        var r = size * 0.25;
+
+        circle.attr({
+            "cx": r + 4,
+            "cy": r + 4,
+            "r": r,
+            "stroke": "black",
+            "stroke-width": "3",
+            "fill": "none",
+            "class": "matrix-input-shape"
+        });
+
+
+        var rect = document.createElementNS(app.ns, "rect");
+        rect = $(rect);
+        rect.attr({
+            "x": this.options.width - 2*r - 4,
+            "y": 4,
+            "height": r*2,
+            "width": r*2,
+            "stroke": "black",
+            "stroke-width": "3",
+            "fill": "none",
+            "class": "matrix-input-shape"
+        });
+
+        svg.append(circle).append(rect);
+
+        this.circle = circle;
+        this.rect = rect;
+        
+        this.setShape("rect", "circle");
+        //todo -> add stopListening
+        this.listenToClick("circle", "rect");
+        this.listenToClick("rect", "circle");
+
+        return this;
+    },
+
+    listenToClick: function(shape, other) {
+        var self = this;
+        this[shape].click(function() {
+            self.setShape(shape, other);
+        });
+    },
+
+    setShape: function(set, unset) {
+        this[unset].attr({
+            "fill-opacity": "0.01",
+            "fill": "none"
+        });
+        this[set].attr({
+            "fill-opacity": "1",
+            "fill": "orange"
+        });
+        this.shape = set;
+    },
+
+    getState: function() {
+        return {
+            shape: this.shape
+        };
+    }
+});
+
+var InputOptionsView = Backbone.View.extend({
+
+    events: {
+        "click button": "onVisualizeClicked"
+    },
+
+    initialize: function(options) {
+        this.options = options;
+        this.views = [];
+    },
+
+    TRANSFORMATION_MATRIX: [
+        [1, 3],
+        [3, 1]
+    ],
+
+    render: function() {
+
+        var row = $("<div class='row'> </div>");
+        this.$el.append(row);
+
+        var speedAndShapeColumn = $("<div class='col-xs-12 col-md-8'> </div>");
+        var speedAndShapeContainer = $("<div class='row'>");
+        var left = $("<div class='col-xs-6'>");
+        var right = $("<div class='col-xs-6'>");
+        speedAndShapeContainer.append(left).append(right);
+        speedAndShapeColumn.append(speedAndShapeContainer);
+        row.append(speedAndShapeColumn);
+
+
+        var speedView = new SpeedView({}).render();
+        right.append(speedView.$el);
+        this.views.push(speedView);
+
+        var shapeView = new MatrixShapeOptionsView({
+            width: right.width(),
+            height: speedView.$el.height()
+        });
+        left.append(shapeView.$el);
+        shapeView.render();
+        this.views.push(shapeView);
+
+
+        var matrixColumn = $("<div class='col-xs-12 col-md-4'> </div>");
+        row.append(matrixColumn);
+
+        var h4 = $("<h4>").addClass("matrix-options-heading").html("Transformation Matrix");
+        matrixColumn.append(h4);
+
+
+        var matrixView = new MatrixInputView({
+            top: h4.outerHeight(true),
+            parent: matrixColumn,
+            matrix: this.TRANSFORMATION_MATRIX
+        });
+        matrixColumn.append(matrixView.$el);
+        matrixView.render();
+        this.views.push(matrixView);
+        
+
+
+        this.renderVisualizeButton(this.$el);
+
+        return this;
+    },
+
+
+    renderVisualizeButton: function(parent) {
+        var button = $("<button class='btn waves-effect waves-light btn-large'>Visualize</button>").css({
+            "width": "100%",
+            "margin-top": "10px"
+        }).attr("id", "matrix-visualize-btn");
+        parent.append(button);
+    },
+
+
+    getState: function() {
+        var state = {};
+        this.views.forEach(function(view) {
+            _.extend(state, view.getState());
+        });
+        return state;
+    },
+
+    onVisualizeClicked: function(evt) {
+        this.options.visualize(this.getState());
+    },
+});
+
+app.gridView = null;
+
+var visualize = function(inputState) {
+    
+    console.log(inputState);
+    if(app.gridView !== null) {
+        app.gridView.remove();
+    }
+
+    var a = range(-1, 1, 0.2);
+    if(inputState.shape === "rect") {
+        axb = cartesianProduct(a, a);
+    }
+    else {
+        axb = concentricCircles(200, 5, 20);
+    }
+
+    //First character of matrices are in uppercase
+    var Transformation = $M(inputState.transformationMatrix);
+    var Input = $M(axb);
+    var Result = Input.multiply(Transformation);
+
+    var options = {
+        timeout: inputState.timeout,
+        renderAxes: false,
+        renderOnResize: false,
+        maxPlotsPerRow: 2,
+        plots: [Input.elements, Result.elements],
+        colors: getColorsArray(axb.length * axb[0].length)
+    };
+    app.gridView = new SvgGridView(options);
+
+    var y = app.gridContainer.offset().top;
+    console.log("scrollTo: ", y);
+    app.gridContainer.append(app.gridView.$el);
+    app.gridView.render();
+    window.scrollTo(0, Math.floor(y));
+    
+};
 
 var render = function() {
+
+    var inputOptionsView = new InputOptionsView({
+        visualize: visualize
+    });
+
+    app.gridContainer = $("<div>");
+    $("#svg-container").append(inputOptionsView.$el).append(app.gridContainer);
+    inputOptionsView.render();
+
+    window.app = app;
+};
+
+
+var ok = function() {
     //initColors();
 
     console.log("hi there sylvester");
+   
 
-    var svg = $("svg");
-    
-    var input    
+    var a = range(-1, 1, 0.2);
+    var axb = cartesianProduct(a, a);
 
-    var a = range(-5, 5, 0.2);
-    //var axb = cartesianProduct(a, a);
-
-    var axb = concentricCircles(200, 5, 5);
+    //var axb = concentricCircles(200, 5, 5);
 
     //console.table(axb);
     var m = [[0.7071, 0.7071], [0.2, 0.7071]]
@@ -496,22 +793,24 @@ var render = function() {
     
     var gridView = new SvgGridView({
         height: 600,
-        renderAxes: true,
-        plots: [two.elements, result, three, [[10, 10]]],
+        renderAxes: false,
+        plots: [two.elements, result],
         colors: getColorsArray(axb.length * axb[0].length),
         maxPlotsPerRow: 2,
         timeout: 0,
         renderOnResize: false
     });
 
-    var inputView = new MatrixInputView({}).render();
-    var rangeView = new RangeView({}).render();
-    $("#svg-container").append(rangeView.$el).append(inputView.$el).append(gridView.$el);
+    var optionsView = new InputOptionsView({});
+    $("#svg-container").append(optionsView.$el);
+    optionsView.render();
 
+    $("#svg-container").append(gridView.$el);
     gridView.render();
 
     window.S = S;
     window.axb = axb;
+    window.ov = optionsView;
     //console.table(result);
 }
 
