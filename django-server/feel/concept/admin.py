538db35a194dde24b03f532afd292e1897e16f51
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-11-19 18:51:43 +0530

    Concept API: Added inline admin for debugging.

--- django-server/feel/concept/admin.py
+++ django-server/feel/concept/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Concept, ConceptSection
 
-admin.site.register(Concept)
-admin.site.register(ConceptSection)
+
+class ConceptSectionInline(admin.StackedInline):
+    model = ConceptSection
+    extra = 2
+
+
+class ConceptAdmin(admin.ModelAdmin):
+    inlines = [ConceptSectionInline]
+    
+
+admin.site.register(Concept, ConceptAdmin)
