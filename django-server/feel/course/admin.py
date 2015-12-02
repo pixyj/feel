54be1bdc28178ad1926f38fa8caea340b649e07a
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-12-03 00:02:05 +0530

    Django Admin: Added Course admin.

--- django-server/feel/course/admin.py
+++ django-server/feel/course/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, CourseConcept, ConceptDependency
+
+
+class CourseConceptInline(admin.StackedInline):
+    model = CourseConcept
+    extra = 0
+
+
+
+class ConceptDependencyInline(admin.StackedInline):
+    model = ConceptDependency
+    extra = 0
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [CourseConceptInline, ConceptDependencyInline]
+    
+
+admin.site.register(Course, CourseAdmin)
