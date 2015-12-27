96f04296f7b070b1e03d17a4c36212b3b65efaaf
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-12-27 23:58:43 +0530

    Loading Circle: Show loading only after waiting for 750ms.

--- client/app/base/js/loading-circle.jsx
+++ client/app/base/js/loading-circle.jsx
@@ -2,7 +2,33 @@ var React = require("react");
 
 var LoadingCircle = React.createClass({
 
+    getInitialState: function() {
+        return {
+            show: false
+        }
+    },
+
+    componentDidMount: function() {
+        var self = this;
+        this._timer = setTimeout(function() {
+            self.setState({
+                show: true
+            })
+        }, 750);
+    },
+
+    componentWillUnmount: function() {
+        clearTimeout(this._timer);
+    },
+
     render: function() {
+
+        if(!this.state.show) {
+            return (
+                <div> </div>
+            );
+        }
+
         return (
             <div className="preloader-wrapper active">
               <div className="spinner-layer spinner-blue-only">
