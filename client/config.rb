5c523f09f5feaee2854531fba4842a9b08b85e0d
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-12-29 14:54:05 +0530

    CSS to SCSS: Migrated app css code to scss

---
+++ client/config.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+
+css_dir = "dist"
+sass_dir = "scss"
+add_import_path "app/base/scss"
+add_import_path "app/code-quiz/scss"
+add_import_path "app/concept/scss"
+add_import_path "app/conceptviz/scss"
+add_import_path "app/course/scss"
+add_import_path "app/quiz/scss"
+add_import_path "app/vendor"
+
+
