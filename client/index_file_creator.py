f9525fcbca3137cc081970b79fc9fd0e0983fc24
Pramod Lakshmanan <pramodliv1@gmail.com>
2015-12-31 19:25:54 +0530

    Client Deployment: Commit suffix is added to min files in production index.html

--- client/index_file_creator.py
+++ client/index_file_creator.py
@@ -1,13 +1,14 @@
 import os
+import argparse
 
 from django.template import Template, Context
 from django.template.engine import Engine
 
 from django.conf import settings
-
 settings.configure(DEBUG=True)
 
-def create_index_file_from_template(mode):
+
+def create_index_file_from_template(mode, commit):
     if mode == "prod":
         options = {
             "DIST_DIR": "/dist/min",
@@ -15,7 +16,8 @@ def create_index_file_from_template(mode):
             "APP_CSS_PREFIX": "app.min",
             "VENDOR_JS_PREFIX": "vendor-min",
             "APP_JS_PREFIX": "app-min",
-            "INDEX_FILE_NAME": "prod-index.html"
+            "INDEX_FILE_NAME": "prod-index.html",
+            "COMMIT": "-{}".format(commit)
         }
     elif mode == "dev":
         options = {
@@ -24,7 +26,8 @@ def create_index_file_from_template(mode):
             "APP_CSS_PREFIX": "app",
             "VENDOR_JS_PREFIX": "vendor",
             "APP_JS_PREFIX": "app",
-            "INDEX_FILE_NAME": "index.html"
+            "INDEX_FILE_NAME": "index.html",
+            "COMMIT": ""
         }
     else:
         raise Exception("Invalid OPS_MODE, {} found".format(mode))
@@ -39,5 +42,19 @@ def create_index_file_from_template(mode):
         f.write(output_string)
 
 
+
 if __name__ == "__main__":
-    create_index_file_from_template(os.sys.argv[1])
+    parser = argparse.ArgumentParser(description='Create index file')
+    parser.add_argument('-m','--mode', help='dev or prod', required=True)
+    parser.add_argument('-c','--commit', help='Git commit', required=False)
+    args = vars(parser.parse_args())
+    
+    mode = args['mode']
+    commit = args['commit']
+    if mode == 'prod' and commit is None:
+        print("Error: --commit option is required in prod mode")
+        exit(1)
+
+    commit = commit or ""
+    create_index_file_from_template(mode, commit)
+
