712a0c6f1f5aec551f7149a7693b6a7e69ea2e21
Pramod Lakshmanan <pramodliv1@gmail.com>
2016-01-02 00:10:21 +0530

    index.html: Moved all rules in <style> tag  to _global.scss or _user-status.scss as appropriate.

--- client/config.rb
+++ client/config.rb
@@ -2,6 +2,8 @@ require 'compass/import-once/activate'
 
 css_dir = "dist"
 sass_dir = "scss"
+
+#todo -> Migrate paths to new format (similar to the user module)
 add_import_path "app/base/scss"
 add_import_path "app/code-quiz/scss"
 add_import_path "app/concept/scss"
@@ -9,5 +11,6 @@ add_import_path "app/conceptviz/scss"
 add_import_path "app/course/scss"
 add_import_path "app/quiz/scss"
 add_import_path "app/vendor"
+add_import_path "app"
 
 
