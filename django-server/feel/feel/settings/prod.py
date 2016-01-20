a785f679793247a7931be93554985b500d11e51b
Pramod Lakshmanan <pramodliv1@gmail.com>
2016-01-20 20:51:13 +0530

    Ops: Add Opbeat tracking in production.

--- django-server/feel/feel/settings/prod.py
+++ django-server/feel/feel/settings/prod.py
@@ -23,3 +23,17 @@ CONN_MAX_AGE = 60
 
 
 STATIC_ROOT = "/home/pramod/feel-client/static-root"
+
+INSTALLED_APPS += (
+    'opbeat.contrib.django',
+)
+
+OPBEAT = {
+    'ORGANIZATION_ID': os.environ['OPBEAT_ORGANIZATION_ID'],
+    'APP_ID': os.environ['OPBEAT_APP_ID'],
+    'SECRET_TOKEN': ['OPBEAT_SECRET_TOKEN'],
+}
+
+MIDDLEWARE_CLASSES = (
+    'opbeat.contrib.django.middleware.OpbeatAPMMiddleware',
+) + MIDDLEWARE_CLASSES
