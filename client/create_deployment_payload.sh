33641f5fb11abe3187a41953f6659dee56306ca3
Pramod Lakshmanan <pramodliv1@gmail.com>
2016-02-10 13:53:03 +0530

    Client Deployment: Add script to upload client assets in one step.

--- client/create_deployment_payload.sh
+++ client/create_deployment_payload.sh
@@ -1,2 +1,6 @@
+./compass-compile-vendor.sh
+./compass-compile-app.sh
+./browserify-app.sh
+gulp minify
+./create-commit-suffix-files.sh 
 python create_deployment_payload.py --commit=`git rev-parse HEAD` -z /Users/pramod/Dropbox/Public/feel-client
-
