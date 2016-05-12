d1b58de4c35a5b683b95052ad9ace79a90198978
Pramod Lakshmanan <pramodliv1@gmail.com>
2016-05-12 17:15:51 +0530

    Client Deployment: Add favicon.png to distribution

--- client/create_deployment_payload.py
+++ client/create_deployment_payload.py
@@ -29,7 +29,7 @@ def create_payload(commit, output_zip_dir):
         exec_command(command)
 
     print("Creating nginx.conf and placing it in {}".format(parent_dir))
-    #todo Make this configurable. And remove your name, dude. 
+    # todo Make this configurable. And remove your name, dude.
     conf = {
         'NGINX_ROOT': "/home/pramod/feel-client",
         'NGINX_LISTEN_PORT': '80',
@@ -42,10 +42,11 @@ def create_payload(commit, output_zip_dir):
     create_index_file_from_template('prod', commit)
     command = "cp {} {}/index.html".format('prod-index.html', parent_dir)
     exec_command(command)
-    command = "cp {} {}".format('dist/retina_dust.png', parent_dir)
+    command = "cp {} {}".format('dist/favicon.png', parent_dir)
     exec_command(command)
     dir_name = 'dist-{}'.format(commit)
-    command = 'zip -r {}/dist-{}.zip {}'.format(output_zip_dir, commit, dir_name)
+    command = 'zip -r {}/dist-{}.zip {}'.format(output_zip_dir,
+                                                commit, dir_name)
     exec_command(command)
     cleanup_min_files()
 
@@ -58,8 +59,9 @@ def cleanup_min_files():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c','--commit', help='Git commit', required=True)
-    parser.add_argument('-z','--output_zip_dir', help='Where do you wanna store the zip?', required=True)
+    parser.add_argument('-c', '--commit', help='Git commit', required=True)
+    parser.add_argument('-z', '--output_zip_dir',
+                        help='Where do you wanna store the zip?',
+                        required=True)
     args = vars(parser.parse_args())
     create_payload(args['commit'], args['output_zip_dir'])
-        
